fe5bb1afd3a61ee42b9ebd0a50693bf5b5425a4a
NutiNaguti <34234176+NutiNaguti@users.noreply.github.com>
2019-01-08 17:22:29 +0400

    Update Response.cs

--- Response.cs
+++ Response.cs
@@ -1,11 +1,28 @@
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace binance_bot
 {
-    class Response
+    public partial class Response
     {
-        public List<decimal[][]> Asks { get; set; }
-        public List<decimal[][]> Bids { get; set; }
+        [JsonProperty("lastUpdateId")]
+        public long LastUpdateId { get; set; }
 
+        [JsonProperty("bids")]
+        public List<List<Ask>> Bids { get; set; }
+
+        [JsonProperty("asks")]
+        public List<List<Ask>> Asks { get; set; }
     }
+
+    public partial struct Ask
+    {
+        public List<object> AnythingArray;
+        public string String;
+
+        public static implicit operator Ask(List<object> AnythingArray) => new Ask { AnythingArray = AnythingArray };
+        public static implicit operator Ask(string String) => new Ask { String = String };
+    }
+
 }
